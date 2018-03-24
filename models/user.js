f95d7b259c7d78b1eda482ee6837de2fadf5e32d
rohanricky <rohanricky0609@gmail.com>
2018-03-24 16:00:27 +0530

    auth verified

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 var Schema = mongoose.Schema;
 
 // create a schema
@@ -16,6 +18,36 @@ var userSchema = new Schema({
   updated_at: Date
 });
 
+userSchema.statics.authenticate = function (username, password, callback) {
+  User.findOne({ username: username })
+    .exec(function (err, user) {
+      if (err) {
+        return callback(err)
+      } else if (!user) {
+        var err = new Error('User not found.');
+        err.status = 401;
+        return callback(err);
+      }
+      bcrypt.compare(password, user.password, function (err, result) {
+        if (result === true) {
+          return callback(null, user);
+        } else {
+          return callback();
+        }
+      })
+    });
+}
+
+userSchema.pre('save', function(next){
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash){
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+});
 // the schema is useless so far
 // we need to create a model using it
 var User = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 var User = require('./models/user');
 const mongoose = require('mongoose');
-
-
+var session = require('express-session');
+// bycrypt , connect-mongo and express-session
 mongoose.connect('mongodb://localhost/blog');
 
 const app=express();
+
+app.use(session({
+  secret: 'Infinity War',
+  resave: true,
+  saveUninitialized: false
+}));
 //Body Parser
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
@@ -17,21 +23,61 @@ app.get('/',(req,res)=>{
 
 app.route('/register')
     .get((req,res,next)=>{
-        res.send('Login template here');
+        res.send('Register template here');
       })
     .post((req,res,next)=>{
-      var name = req.body.name;
-      var username = req.body.username;
-      var password = req.body.password;
-      var new_user = new User({
-        name: name,
-        username: username,
-        password: password
-      });
-      new_user.save((err)=>{
-        if(err) throw err;
+      if(req.body.name && req.body.username && req.body.password){
+        var userData = {
+          name: req.body.name,
+          username: req.body.username,
+          password: req.body.password,
+        }
+
+        User.create(userData, function (err, user) {
+          if (err) {
+            return next(err)
+          } else {
+            req.session.userId = user._id;
+            return res.redirect('/profile'); // redirect to login / profile directly
+          }
         });
-      res.send(name+" "+username+" "+password);
+      }
       });
+
+app.route('/login')
+    .get((req,res,next)=>{
+      res.send('Login template here');
+    })
+    .post((req,res,next)=>{
+      if(req.body.username && req.body.password){
+        User.authenticate(req.body.username, req.body.password, function (error, user) {
+          if (error || !user) {
+            var err = new Error('Wrong email or password.');
+            err.status = 401;
+            return next(err);
+          } else {
+            req.session.userId = user._id;
+            return res.redirect('/profile');
+      }
+    });
+      }
+    });
+
+app.get('/profile', function (req, res, next) {
+    User.findById(req.session.userId)
+        .exec(function (error, user) {
+          if (error) {
+            return next(error);
+          } else {
+            if (user === null) {
+              var err = new Error('Not authorized! Go back!');
+              err.status = 400;
+              return next(err);
+            } else {
+              return res.send('You are logged in: '+user.username)
+            }
+          }
+        });
+    });
 
 app.listen(5000,() => console.log('Example app listening on port 5000!'))
