d591f4af22d4ed2346b42733891b7834f88ce210
rohanricky <rohanricky0609@gmail.com>
2018-03-25 10:19:55 +0530

    deployed database

--- router/blog.js
+++ router/blog.js
@@ -3,13 +3,14 @@ var router = express.Router();
 var session = require('express-session');
 var Blog = require('../models/blogSchema');
 const bodyParser = require('body-parser');
+var path = require('path');
 
 router.use(bodyParser.urlencoded({extended:true}));
 router.use(bodyParser.json());
 
 router.route('/')
     .get((req,res,next)=>{
-      res.send('Blog post edit template');
+      res.sendFile(path.join(__dirname, '../html', 'blogpost.html'));
     })
     .post((req,res,next)=>{
       if(req.body.title && req.body.content){
